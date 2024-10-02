994bd1e024b43cec8cd4f076c144bfb55cf2f6a7
renu chandraker <90048222+renu100@users.noreply.github.com>
2024-10-02 14:25:45 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,6 @@ if sudo lsof -i :8080; then
   echo "Port 8080 is in use. Stopping the container using it..."
   container_id=$(sudo docker ps -q --filter "expose=8080")
   sudo docker stop $container_id || true
+fi
 sudo docker pull 942731209985.dkr.ecr.ap-south-1.amazonaws.com/poc-repo:latest
 sudo docker run -d --name flask_app-container -p 8080:8080 942731209985.dkr.ecr.ap-south-1.amazonaws.com/poc-repo:latest
