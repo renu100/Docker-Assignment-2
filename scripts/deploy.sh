c9dfd925ed4b2a283360814dd2c2c7adf5f6ba76
renu chandraker <90048222+renu100@users.noreply.github.com>
2024-10-02 14:18:25 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd /home/ec2-user/flask-app
 sudo docker rm -f flask-app-container
+aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin 942731209985.dkr.ecr.ap-south-1.amazonaws.com
+if sudo lsof -i :8080; then
+  echo "Port 8080 is in use. Stopping the container using it..."
+  container_id=$(sudo docker ps -q --filter "expose=8080")
+  sudo docker stop $container_id || true
 sudo docker pull 942731209985.dkr.ecr.ap-south-1.amazonaws.com/poc-repo:latest
 sudo docker run -d --name flask_app-container -p 8080:8080 942731209985.dkr.ecr.ap-south-1.amazonaws.com/poc-repo:latest
