602ffddcd3d5d2fd82dafc90995d007013248d3c
renu chandraker <90048222+renu100@users.noreply.github.com>
2024-10-02 14:08:14 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/flask-app
-sudo docker rm -f flask-app
+sudo docker rm -f flask-app-container
 sudo docker pull 942731209985.dkr.ecr.ap-south-1.amazonaws.com/poc-repo:latest
-sudo docker run -d -p 8080:8080 942731209985.dkr.ecr.ap-south-1.amazonaws.com/poc-repo:latest
+sudo docker run -d --name flask_app-container -p 8080:8080 942731209985.dkr.ecr.ap-south-1.amazonaws.com/poc-repo:latest
